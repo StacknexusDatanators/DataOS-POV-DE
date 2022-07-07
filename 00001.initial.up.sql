19de22539372a263b46a75a27ca36e3935450495
bantti <neha.rahangadale@stacknexus.io>
2022-07-07 19:30:55 +0530

    added postgres beacon yaml file

---
+++ 00001.initial.up.sql
@@ -0,0 +1,61 @@
+create table customer(
+  __metadata_dataos_run_mapper_id STRING,
+customer_index STRING,
+id STRING,
+salutation STRING,
+first_name STRING,
+last_name STRING,
+gender STRING,
+phone_number STRING,
+email_id STRING,
+birthdate STRING,
+age STRING,
+education_level STRING,
+marital_status STRING,
+number_of_children STRING,
+occupation STRING,
+annual_income STRING,
+employment_status STRING,
+hobbies STRING,
+home_ownership STRING,
+degree_of_loyalty STRING,
+benefits_sought STRING,
+personality STRING,
+user_status STRING,
+social_class STRING,
+lifestyle STRING,
+city_id STRING,
+mailing_street STRING
+);
+
+
+
+
+
+
+
+
+create table product(
+  __metadata_dataos_run_mapper_id STRING,
+  productsku STRING,
+  v2productname STRING,
+  v2productcategory STRING,
+  productvariant STRING,
+  productbrand STRING,
+  productrevenue STRING,
+  productprice STRING,
+  productstock STRING
+);
+
+
+
+
+create table transactions(
+  __metadata_dataos_run_mapper_id STRING,
+  productsku STRING,
+  customer_id STRING,
+  quantity STRING,
+  type STRING,
+  productprice STRING,
+  time STRING
+);
